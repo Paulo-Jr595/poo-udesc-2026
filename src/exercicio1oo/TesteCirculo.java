5480d8eeda792729671d11575004bd3607031297
Paulo-Lr595 <junior_pscordeiro@hotmail.com>
2026-09-22 13:02:02 -0300

    Exercicio Circulo e respectivo Teste

---
+++ src/exercicio1oo/TesteCirculo.java
@@ -0,0 +1,11 @@
+package exercicio1oo;
+
+public class TesteCirculo {
+    public static void main(String[] args) {
+        Circulo circulo = new Circulo();
+        circulo.raio = 0.77;
+
+        System.out.println("Raio: " + circulo.raio);
+
+    }
+}
--- src/exercicio1oo/TesteRetangulo.java
+++ src/exercicio1oo/TesteRetangulo.java
@@ -1,10 +1,10 @@
 package exercicio1oo;
 
 public class TesteRetangulo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Retangulo retangulo = new Retangulo();
-        retangulo.largura= 0.65;
-        retangulo.altura= 2.05;
+        retangulo.largura = 0.65;
+        retangulo.altura = 2.05;
 
         System.out.println("Largura: " + retangulo.largura);
         System.out.println("Altura: " + retangulo.altura);
